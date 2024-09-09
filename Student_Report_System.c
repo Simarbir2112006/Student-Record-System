48a3fda5be9df5584498b7355af19a08e748f52e
Simarbir Singh Sandhu <62996303+Simarbir2112006@users.noreply.github.com>
2024-09-09 18:06:24 +0530

    Student_Report_System.c

--- Student_Report_System.c
+++ Student_Report_System.c
@@ -1,8 +1,6 @@
 #include <ctype.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
 #include <stdbool.h>
 
 // Defining max length for the name and branch abrivations
